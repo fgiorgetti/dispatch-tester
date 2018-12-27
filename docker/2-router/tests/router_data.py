a2693b00b8b3b5c79e0126446e823d4f87c0f282
Fernando Giorgetti <fgiorget@redhat.com>
2018-12-27 17:35:55 -0200

    Updated thresholds for memory usage

--- docker/2-router/tests/router_data.py
+++ docker/2-router/tests/router_data.py
@@ -99,170 +99,170 @@ class AllocatorInfo(object):
     # Key is the typeName.
     # Any metric can be added here as needed.
     #
-    THRESHOLDS = {
-        'qd_bitmask_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qd_buffer_t': {
-            'totalAllocFromHeap': 768,
-            'heldByThreads': 512
-        },
-        'qd_composed_field_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qd_composite_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qd_connection_t': {
-            'totalAllocFromHeap': 128,
-            'heldByThreads': 128
-        },
-        'qd_connector_t': {
-            'totalAllocFromHeap': 128,
-            'heldByThreads': 128
-        },
-        'qd_deferred_call_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qd_hash_handle_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qd_hash_item_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qd_iterator_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qd_link_ref_t': {
-            'totalAllocFromHeap': 384,
-            'heldByThreads': 256
-        },
-        'qd_link_t': {
-            'totalAllocFromHeap': 768,
-            'heldByThreads': 768
-        },
-        'qd_listener_t': {
-            'totalAllocFromHeap': 128,
-            'heldByThreads': 128
-        },
-        'qd_log_entry_t': {
-            'totalAllocFromHeap': 2048,
-            'heldByThreads': 2048
-        },
-        'qd_message_content_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qd_message_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qd_node_t': {
-            'totalAllocFromHeap': 128,
-            'heldByThreads': 128
-        },
-        'qd_parsed_field_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qd_parsed_turbo_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qd_parse_node_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qd_pn_free_link_session_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qdr_action_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 256
-        },
-        'qdr_address_config_t': {
-            'totalAllocFromHeap': 128,
-            'heldByThreads': 128
-        },
-        'qdr_address_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qdr_connection_info_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qdr_connection_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qdr_connection_work_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qdr_delivery_ref_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qdr_delivery_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qdr_connection_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qdr_error_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qdr_field_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qdr_general_work_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qdr_link_ref_t': {
-            'totalAllocFromHeap': 1536,
-            'heldByThreads': 1536
-        },
-        'qdr_link_t': {
-            'totalAllocFromHeap': 1024,
-            'heldByThreads': 1024
-        },
-        'qdr_link_work_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qdr_node_t': {
-            'totalAllocFromHeap': 128,
-            'heldByThreads': 128
-        },
-        'qdr_query_t': {
-            'totalAllocFromHeap': 128,
-            'heldByThreads': 128
-        },
-        'qdr_terminus_t': {
-            'totalAllocFromHeap': 512,
-            'heldByThreads': 512
-        },
-        'qd_timer_t': {
-            'totalAllocFromHeap': 256,
-            'heldByThreads': 256
-        },
-        'qdtm_router_t': {
-            'totalAllocFromHeap': 128,
-            'heldByThreads': 128
-        }
+THRESHOLDS = {
+    'qd_bitmask_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qd_buffer_t': {
+        'totalAllocFromHeap': 768,
+        'heldByThreads': 512
+    },
+    'qd_composed_field_t': {
+        'totalAllocFromHeap': 256,
+        'heldByThreads': 256
+    },
+    'qd_composite_t': {
+        'totalAllocFromHeap': 256,
+        'heldByThreads': 256
+    },
+    'qd_connection_t': {
+        'totalAllocFromHeap': 128,
+        'heldByThreads': 128
+    },
+    'qd_connector_t': {
+        'totalAllocFromHeap': 128,
+        'heldByThreads': 128
+    },
+    'qd_deferred_call_t': {
+        'totalAllocFromHeap': 256,
+        'heldByThreads': 256
+    },
+    'qd_hash_handle_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qd_hash_item_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qd_iterator_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qd_link_ref_t': {
+        'totalAllocFromHeap': 384,
+        'heldByThreads': 256
+    },
+    'qd_link_t': {
+        'totalAllocFromHeap': 768,
+        'heldByThreads': 768
+    },
+    'qd_listener_t': {
+        'totalAllocFromHeap': 128,
+        'heldByThreads': 128
+    },
+    'qd_log_entry_t': {
+        'totalAllocFromHeap': 2048,
+        'heldByThreads': 2048
+    },
+    'qd_message_content_t': {
+        'totalAllocFromHeap': 256,
+        'heldByThreads': 256
+    },
+    'qd_message_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qd_node_t': {
+        'totalAllocFromHeap': 128,
+        'heldByThreads': 128
+    },
+    'qd_parsed_field_t': {
+        'totalAllocFromHeap': 384,
+        'heldByThreads': 384
+    },
+    'qd_parsed_turbo_t': {
+        'totalAllocFromHeap': 256,
+        'heldByThreads': 256
+    },
+    'qd_parse_node_t': {
+        'totalAllocFromHeap': 256,
+        'heldByThreads': 256
+    },
+    'qd_pn_free_link_session_t': {
+        'totalAllocFromHeap': 256,
+        'heldByThreads': 256
+    },
+    'qdr_action_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 256
+    },
+    'qdr_address_config_t': {
+        'totalAllocFromHeap': 128,
+        'heldByThreads': 128
+    },
+    'qdr_address_t': {
+        'totalAllocFromHeap': 256,
+        'heldByThreads': 256
+    },
+    'qdr_connection_info_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qdr_connection_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qdr_connection_work_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qdr_delivery_ref_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qdr_delivery_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qdr_error_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qdr_field_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qdr_forward_deliver_info_t': {
+        'totalAllocFromHeap': 128,
+        'heldByThreads': 128
+    },
+    'qdr_general_work_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qdr_link_ref_t': {
+        'totalAllocFromHeap': 1536,
+        'heldByThreads': 1536
+    },
+    'qdr_link_t': {
+        'totalAllocFromHeap': 1024,
+        'heldByThreads': 1024
+    },
+    'qdr_link_work_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qdr_node_t': {
+        'totalAllocFromHeap': 128,
+        'heldByThreads': 128
+    },
+    'qdr_query_t': {
+        'totalAllocFromHeap': 128,
+        'heldByThreads': 128
+    },
+    'qdr_terminus_t': {
+        'totalAllocFromHeap': 512,
+        'heldByThreads': 512
+    },
+    'qd_timer_t': {
+        'totalAllocFromHeap': 256,
+        'heldByThreads': 256
+    },
+    'qdtm_router_t': {
+        'totalAllocFromHeap': 128,
+        'heldByThreads': 128
     }
+}
 
